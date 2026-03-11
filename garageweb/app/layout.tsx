d06d3e4d56050fe6bd1d7b8ed6bcb6bd925e2d82
Valentino Olmedo <valenolme@gmail.com>
2026-03-10 23:44:58 -0300

    limpieza de codigo, optimizacion seo

--- garageweb/app/layout.tsx
+++ garageweb/app/layout.tsx
@@ -17,6 +17,7 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://elgarage.com.ar"),
   title: {
     template: 'El Garage | %s',
     default: 'El Garage Automóviles',
